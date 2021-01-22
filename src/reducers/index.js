81084bac730831abe8b96d91c310ecb860082a76
ChrisColePitre <ChrisColePitre@gmail.com>
2021-01-22 14:23:12 -0500

    fixing stupid case sensitivity error

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import itemsReducer from "../features/items/itemsSlice";
-// import photosReducer from "../features/photos/photosSlice";
-
-export default combineReducers({
-  // photos: photosReducer,
-  items: itemsReducer
-});
